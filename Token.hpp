53f1eee20559cf6b15515cb1a3812ac11c5fd20c
fyz.m <237607300+fyz-m@users.noreply.github.com>
2026-05-24 10:50:32 +0400

    Move TokenType enums

--- Token.hpp
+++ Token.hpp
@@ -1,6 +1,26 @@
 #pragma once
-#include "TokenType.hpp"
 #include <string>
+
+enum class TokenType {
+  // Single-character tokens.
+  LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+  COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+  // One or two character tokens.
+  BANG, BANG_EQUAL,
+  EQUAL, EQUAL_EQUAL,
+  GREATER, GREATER_EQUAL,
+  LESS, LESS_EQUAL,
+
+  // Literals.
+  IDENTIFIER, STRING, NUMBER,
+
+  // Keywords.
+  AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+  PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+  _EOF
+};
 
 class Token {
 
@@ -16,11 +36,12 @@ class Token {
 
     Token(
           TokenType type, 
-          std::string lexeme, 
-          std::string literal,
+          std::string&& lexeme, 
+          std::string&& literal,
           int line
          );
 
     std::string toString();
     
 };
+
