c4f195c3c955c986f1d3c627216638496a8c450d
fyz.m <237607300+fyz-m@users.noreply.github.com>
2026-05-31 18:04:20 +0400

    Add declarations

--- src/Parser.hpp
+++ src/Parser.hpp
@@ -1,18 +1,46 @@
 #pragma once
+#include "Expr.hpp"
 #include "Token.hpp"
+#include <initializer_list>
+#include <memory>
 #include <vector>
 
 class Parser {
 
     private:
 
-        const std::vector<Token> tokens;
+        std::vector<Token> tokens;
         int current {0};
     
     public:
 
-        Parser(const std::vector<Token>&& tokens);
+        Parser(const std::vector<Token>& tokens);
     
     private:
-        
+    
+        std::unique_ptr<Expr> expression();
+
+        std::unique_ptr<Expr> equality();
+
+        std::unique_ptr<Expr> comparison();
+
+        std::unique_ptr<Expr> term();
+
+        std::unique_ptr<Expr> factor();
+
+        std::unique_ptr<Expr> unary();
+
+        std::unique_ptr<Expr> primary();
+
+        bool match(std::initializer_list<TokenType> types);
+
+        bool isAtEnd();
+
+        Token& peek();
+
+        Token& previous();
+
+        Token& advance();
+
+
 };
