05c0447c1ad9542b40a79c71af0329304d163d0e
fyz.m <237607300+fyz-m@users.noreply.github.com>
2026-05-31 11:26:38 +0400

    Fixed cli args

--- src/main.cpp
+++ src/main.cpp
@@ -5,20 +5,19 @@
 int main(int argc, char *argv[])
 {
 
-  Lox lox;
 
-  if (argc > 2)
+  if (argc > 2 || argc < 2)
   {
     std::cout << "Usage: cpplox [script]" << std::endl;
     return 64;
   } 
   else if (argc == 2) 
   {
-    lox.runFile(argv[1]);
+    Lox::runFile(argv[1]);
   }
   else 
   {
-    lox.runPrompt(); 
+    Lox::runPrompt(); 
   }
 }
 
