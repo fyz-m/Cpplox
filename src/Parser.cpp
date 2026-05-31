7f702a44210057d07ec5efb3af47abb127e7a221
fyz.m <237607300+fyz-m@users.noreply.github.com>
2026-05-31 18:53:10 +0400

    add equality

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -1,5 +1,6 @@
 #include "Parser.hpp"
 #include "Token.hpp"
+#include <memory>
 #include <utility>
 #include <vector>
 
@@ -13,8 +14,14 @@ std::unique_ptr<Expr> Parser::expression() {
 
 std::unique_ptr<Expr> Parser::equality() {
     
+    // Left child node of the binary node
     auto expr = comparison();
 
+    while (match({TokenType::BANG_EQUAL,TokenType::EQUAL_EQUAL})) {
+          Token operator_ = previous(); 
+          auto right = comparison(); // right child node
+          expr = std::make_unique<Binary>(std::move(expr), std::move(operator_), std::move(right));                             
+    }
     return expr;
 }
 
@@ -39,23 +46,35 @@ std::unique_ptr<Expr> Parser::primary() {
 }
 
 
-bool match(std::initializer_list<TokenType> types) {
+bool Parser::match(std::initializer_list<TokenType> expected_types) {
 
+  for (auto expected_type : expected_types) {
+    
+      if (check(expected_type)) {
+         advance();
+         return true;
+      }
+  }
+    return false;
+}
+
+bool Parser::check(TokenType expected_type) {
+    return peek().type == expected_type;
 }
 
 bool Parser::isAtEnd() {
-    if (peek().type == TokenType::_EOF) return false;
-    return true;
+    return peek().type == TokenType::_EOF;
 }
 
 Token& Parser::peek() {
     return tokens[current];
 }
-
+ 
 Token& Parser::previous() {
     return tokens[current-1];
 }
 
 Token& Parser::advance() {
-    return tokens[current++];
+    if (!isAtEnd()) current++;
+    return previous();
 }
